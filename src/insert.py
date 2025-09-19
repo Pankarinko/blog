502657fe22a330ceeca446c69be8d278b9c73db2
Pankarinko <pankarinko@gmail.com>
2025-09-19 15:39:49 +0200

    Rename public to docs

--- src/insert.py
+++ src/insert.py
@@ -6,7 +6,7 @@ path = "base.html"
 
 def insert(path, title):
     content_path =  "blog/" + title + ".md"
-    subprocess.run(["pandoc", "--template=base.template", "--highlight-style=zenburn", content_path, "-o", "../public/" + title + ".html"])
+    subprocess.run(["pandoc", "--template=base.template", "--highlight-style=zenburn", content_path, "-o", "../docs/" + title + ".html"])
 
 def create_pages():
     list = os.listdir("blog")
